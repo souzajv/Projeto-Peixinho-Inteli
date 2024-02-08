30b503b459bf5bc3fe6e5650f014367696e771c9
João Souza <51976773+souzajv@users.noreply.github.com>
2024-02-07 21:34:51 -0300

    finalização do projeto
    finalização do projeto com todos os requisitos adicionais.

--- script.js
+++ script.js
@@ -11,6 +11,25 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
-function preload() {}
-function create() {}
-function update() {}
+
+var movimentoPeixe
+
+function preload() {
+    this.load.image('mar', 'assets/bg_azul-escuro.png');
+    this.load.image('logo', 'assets/logo-inteli_branco.png');
+    this.load.image('peixe', 'assets/peixes/peixinho_rosa.png');
+    this.load.image('mamaco', 'assets/ultimo-romantico.jfif');
+}
+function create() {
+    this.add.image(400, 300, 'mar')
+    this.add.image(400,525, 'logo').setScale(0.5);
+    this.add.image(30, 30, 'mamaco').setScale(0.2);
+    movimentoPeixe = this.add.image(400, 300, 'peixe');
+    movimentoPeixe.setFlip(true, false);
+}
+
+function update() {
+    movimentoPeixe.x = this.input.x;
+    movimentoPeixe.y = this.input.y;
+}
+
