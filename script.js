523c1877673789b6eed13014a7cfa03bbadb526e
João Souza <51976773+souzajv@users.noreply.github.com>
2024-02-08 08:55:35 -0300

    Entrega final
    Alteração no item adicional.

--- script.js
+++ script.js
@@ -18,12 +18,12 @@ function preload() {
     this.load.image('mar', 'assets/bg_azul-escuro.png');
     this.load.image('logo', 'assets/logo-inteli_branco.png');
     this.load.image('peixe', 'assets/peixes/peixinho_rosa.png');
-    this.load.image('mamaco', 'assets/ultimo-romantico.jfif');
+    this.load.image('tartaruga', 'assets/tartaruga.png');
 }
 function create() {
     this.add.image(400, 300, 'mar')
     this.add.image(400,525, 'logo').setScale(0.5);
-    this.add.image(30, 30, 'mamaco').setScale(0.2);
+    this.add.image(600, 350, 'tartaruga').setScale(0.6);
     movimentoPeixe = this.add.image(400, 300, 'peixe');
     movimentoPeixe.setFlip(true, false);
 }
